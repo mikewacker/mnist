e9aedf5dff6e308947e5439300012e2b976262b6
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2020-09-23 02:33:21 -0600

    add activations

--- nn_activations.py
+++ nn_activations.py
@@ -1,5 +1,88 @@
 import numpy as np
 
+def activation(fn):
+    """Creates an activation using the provided function."""
+    if fn == "relu":
+        return _ReluActivation()
+    if fn == "sigmoid":
+        return _SigmoidActivation()
+    if fn == "tanh":
+        return _TanhActivation()
+    msg = "unknown activation function: {}".format(fn)
+    raise ValueError(msg)
+
+####
+# Activations
+####
+
+class _ReluActivation(object):
+    """ReLU activation."""
+
+    def forward(self, Z):
+        """Feeds the inputs forward."""
+        # Feed forward.
+        mask = Z >= 0
+        A = Z * mask
+
+        # Cache and return.
+        self._mask = mask
+        return A
+
+    def backward(self, dA):
+        """Back-propagates the output gradients."""
+        # Retrieve from cache.
+        mask = self._mask
+
+        # Back-propagate.
+        dZ = dA * mask
+        return dZ
+
+class _SigmoidActivation(object):
+    """Sigmoid activation."""
+
+    def forward(self, Z):
+        """Feeds the inputs forward."""
+        # Feed forward.
+        A = _sigmoid(Z)
+
+        # Cache and return.
+        self._A = A
+        return A
+
+    def backward(self, dA):
+        """Back-propagates the output gradients."""
+        # Retrieve from cache.
+        A = self._A
+
+        # Back-propagate.
+        dZ = dA * A * (1 - A)
+        return dZ
+
+class _TanhActivation(object):
+    """Hyperbolic tangent activation."""
+
+    def forward(self, Z):
+        """Feeds the inputs forward."""
+        # Feed forward.
+        A = np.tanh(Z)
+
+        # Cache and return.
+        self._A = A
+        return A
+
+    def backward(self, dA):
+        """Back-propagates the output gradients."""
+        # Retrieve from cache.
+        A = self._A
+
+        # Back-propagate.
+        dZ = dA * (1 - np.square(A))
+        return dZ
+
+####
+# Activation functions
+####
+
 def _sigmoid(Z):
     """Applies sigmoid activation to each element."""
     A = 1 / (1 + np.exp(-Z))
--- nn_activations_test.py
+++ nn_activations_test.py
@@ -1,19 +1,60 @@
 import unittest
-import nn_layers
+import nn_activations
 
 import numpy as np
 
-class NNLayersTestCase(unittest.TestCase):
+import gradient_checking
+
+class NNActivationsTestCase(unittest.TestCase):
+
+    def testReluActivation_Forward(self):
+        activation = nn_activations.activation("relu")
+        Z = np.array([[1.0, -2.0], [0.0, 0.25]])
+        A = activation.forward(Z)
+        A_expected = np.array([[1.0, 0.0], [0.0, 0.25]])
+        self.assertTrue((A == A_expected).all())
+
+    def testReluActivation_GradientCheck(self):
+        self._testActivation_GradientCheck("relu")
+
+    def testSigmoidActivation_GradientCheck(self):
+        self._testActivation_GradientCheck("sigmoid")
+
+    def testTanhActivation_GradientCheck(self):
+        self._testActivation_GradientCheck("tanh")
+
+    def _testActivation_GradientCheck(self, fn):
+        self._activation = nn_activations.activation(fn)
+        np.random.seed(195262)
+        Z = np.random.normal(0, 1, (3, 3))
+        diff, _, _ = gradient_checking.check(
+            self._activationCost, self._activationGradients, Z)
+        self.assertLess(diff, 1e-7)
+
+    def _activationCost(self, Z):
+        A = self._activation.forward(Z)
+        cost = np.sum(A)
+        return cost
+
+    def _activationGradients(self, Z):
+        A = self._activation.forward(Z)
+        dA = np.ones(A.shape)
+        dZ = self._activation.backward(dA)
+        return (dZ,)
+
+    def testActivationError_UnknownFunction(self):
+        with self.assertRaises(ValueError):
+            nn_activations.activation("dne")
 
     def testSigmoid(self):
         log_odds = np.log(np.array([[3.0, 4.0], [1.0, 0.25]]))
-        A = nn_layers._sigmoid(log_odds)
+        A = nn_activations._sigmoid(log_odds)
         expected_prob = np.array([[0.75, 0.80], [0.50, 0.20]])
         self.assertArraysEqual(A, expected_prob)
 
     def testSoftmax(self):
         log_odds = np.log(np.array([[1.0, 3.0, 1.0], [2.0, 5.0, 3.0]]))
-        A = nn_layers._softmax(log_odds)
+        A = nn_activations._softmax(log_odds)
         expected_prob = np.array([[0.2, 0.6, 0.2], [0.2, 0.5, 0.3]])
         self.assertArraysEqual(A, expected_prob)
 
