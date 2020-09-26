104cba7b6ebe362b56de5c623145c35692edde0b
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2020-09-26 14:18:19 -0600

    create layers

---
+++ nn_layers.py
@@ -0,0 +1,184 @@
+import _nn_units
+import _nn_activations
+
+def dense(n_prev, n, *, activation="relu"):
+    """Creates a dense, fully-connected layer.
+
+    Args:
+        n_prev: number of inputs from the previous layer
+        n: number of nodes for this layer
+        activation: activation function for this layer,
+            one of {sigmoid, tanh, relu}
+    """
+    unit = _nn_units.dense(n_prev, n)
+    activation = _nn_activations.activation(activation)
+    return _ActivatedLayer(unit, activation)
+
+def convolution_2d(
+        n_H_prev, n_W_prev, n_C_prev, n_C, *,
+        kernel_size, stride=1, padding=0, activation="relu"):
+    """Creates a 2D convulational layer.
+
+    Args:
+        n_H_prev: height of input
+        n_W_prev: width of input
+        n_C_prev: number of input channels
+        n_C: number of output channels
+        kernel_size: size of the kernel
+        stride: stride length for the kernel
+        padding: size of pad to add to each side
+        activation: activation function for this layer,
+            one of {sigmoid, tanh, relu}
+    """
+    unit = _nn_units.convolution_2d(
+        n_H_prev, n_W_prev, n_C_prev, n_C,
+        kernel_size=kernel_size, stride=stride, padding=padding)
+    activation = _nn_activations.activation(activation)
+    return _ActivatedLayer(unit, activation)
+
+def max_pool_2d(
+        n_H_prev, n_W_prev, n_C_prev, *, pool_size, stride=0):
+    """Creats a 2D max-pooling layer.
+
+    Args:
+        n_H_prev: height of input
+        n_W_prev: width of input
+        n_C_prev: number of input channels
+        pool_size: size of the pool
+        stride: stride length for the pool, or 0 to match the pool size
+    """
+    return _nn_units.max_pool_2d(
+        n_H_prev, n_W_prev, n_C_prev, pool_size=pool_size, stride=stride)
+
+def flatten(shape_in):
+    """Creates a layer to flatten the inputs.
+
+    Args:
+        shape_in: input shape, excluding the number of samples
+    """
+    return _nn_units.flatten(shape_in)
+
+def binary_output(n_prev):
+    """Creates a binary output layer.
+
+    Args:
+        n_prev: number of inputs from the previous layer
+    """
+    unit = _nn_units.dense(n_prev, 1)
+    output = _nn_activations.binary_output()
+    return _OutputLayer(unit, output)
+
+def multiclass_output(n_prev, C):
+    """Creates a multi-class output layer.
+
+    Args:
+        n_prev: number of inputs from the previous layer
+        C: number of output classes
+    """
+    unit = _nn_units.dense(n_prev, C)
+    output = _nn_activations.multiclass_output(C)
+    return _OutputLayer(unit, output)
+
+"""
+Hidden layers can be created in one of two ways:
+
+*   using a unit by itself
+*   composing a unit and an activation via _ActivatedLayer
+
+An output layer can be created in one way:
+
+*   composing a unit and an output via _OutputLayer
+"""
+
+####
+# Layer composition
+####
+
+class _ActivatedLayer(object):
+    """Builds a hidden layer from a unit an activation function."""
+
+    def __init__(self, unit, activation):
+        """Initializes the layer."""
+        self._unit = unit
+        self._activation = activation
+
+    @property
+    def shape_in(self):
+        """Gets the input shape, excluding the number of samples."""
+        return self._unit.shape_in
+
+    @property
+    def shape_out(self):
+        """Gets the output shape, excluding the number of samples."""
+        return self._unit.shape_out
+
+    @property
+    def weights(self):
+        """Gets the weights."""
+        return self._unit.weights
+
+    @weights.setter
+    def weights(self, value):
+        """Sets the weights."""
+        self._unit.weights = value
+
+    def load_weights(self, nn_dict, index):
+        """Loads pre-trained weights from a dictionary."""
+        self._unit.load_weights(nn_dict, index)
+
+    def save_weights(self, nn_dict, index):
+        """Saves trained weights to a dictionary."""
+        self._unit.save_weights(nn_dict, index)
+
+    def forward(self, A_prev):
+        """Feeds the inputs forward."""
+        Z = self._unit.forward(A_prev)
+        return self._activation.forward(Z)
+
+    def backward(self, dA, chained):
+        """Back-propagates the output gradients."""
+        dZ = self._activation.backward(dA)
+        return self._unit.backward(dZ, chained)
+
+class _OutputLayer(_ActivatedLayer):
+    """Builds an output layer from a unit and output."""
+
+    def __init__(self, unit, output):
+        """Initializes the layer."""
+        super().__init__(unit, output)
+        self._output = output
+
+    @property
+    def is_multiclass(self):
+        """Determines if the output is a multiclass output."""
+        return self._output.is_multiclass
+
+    @property
+    def C(self):
+        """Gets the number of classes."""
+        return self._output.C
+
+    @property
+    def Y(self):
+        """Gets the true labels."""
+        return self._output.Y
+
+    @Y.setter
+    def Y(self, value):
+        """Sets the true labels."""
+        self._output.Y = value
+
+    @property
+    def cost(self):
+        """Gets the cost for the predictions."""
+        return self._output.cost
+
+    @property
+    def pred(self):
+        """Gets the predicted labels."""
+        return self._output.pred
+
+    @property
+    def prob(self):
+        """Gets the predicted probabilities."""
+        return self._output.prob
---
+++ nn_layers_test.py
@@ -0,0 +1,126 @@
+import unittest
+import nn_layers
+
+import numpy as np
+
+import gradient_checking
+
+class NNLayersTestCase(unittest.TestCase):
+
+    def setUp(self):
+        np.random.seed(848131)
+
+    ####
+    # Layers
+    ####
+
+    def testDenseLayer(self):
+        layer = nn_layers.dense(10, 4)
+        self.assertEqual(layer.shape_in, (10,))
+        self.assertEqual(layer.shape_out, (4,))
+        self._testActivatedLayer_GradientCheck(layer)
+
+    def testConvolution2DLayer(self):
+        layer = nn_layers.convolution_2d(5, 5, 3, 6, kernel_size=3, padding=1)
+        self.assertEqual(layer.shape_in, (5, 5, 3))
+        self.assertEqual(layer.shape_out, (5, 5, 6))
+        self._testActivatedLayer_GradientCheck(layer)
+
+    def testMaxPool2DLayer(self):
+        layer = nn_layers.max_pool_2d(4, 4, 3, pool_size=2)
+        self.assertEqual(layer.shape_in, (4, 4, 3))
+        self.assertEqual(layer.shape_out, (2, 2, 3))
+        self._testActivatedLayer_GradientCheck(layer)
+
+    def testFlattenLayer(self):
+        layer = nn_layers.flatten((2, 2, 4))
+        self.assertEqual(layer.shape_in, (2, 2, 4))
+        self.assertEqual(layer.shape_out, (16,))
+        self._testActivatedLayer_GradientCheck(layer)
+
+    def testBinaryOutpuLayer(self):
+        layer = nn_layers.binary_output(10)
+        self.assertEqual(layer.shape_in, (10,))
+        self.assertEqual(layer.shape_out, (1,))
+        self._testOutputLayer_GradientCheck(layer)
+
+    def testMulticlassOutputLayer(self):
+        layer = nn_layers.multiclass_output(10, 4)
+        self.assertEqual(layer.shape_in, (10,))
+        self.assertEqual(layer.shape_out, (4,))
+        self._testOutputLayer_GradientCheck(layer)
+
+    def _testActivatedLayer_GradientCheck(self, layer):
+        A_prev = self._createAPrev(layer)
+        self._layer = layer
+        diff, _, _ = gradient_checking.check(
+            self._activatedLayerCost, self._activatedLayerGradients,
+            A_prev, *layer.weights)
+        self.assertLess(diff, 1e-7)
+
+    def _testOutputLayer_GradientCheck(self, layer):
+        layer.Y = self._createY(layer)
+        A_prev = self._createAPrev(layer)
+        self._layer = layer
+        diff, _, _ = gradient_checking.check(
+            self._outputLayerCost, self._outputLayerGradients,
+            A_prev, *layer.weights)
+        self.assertLess(diff, 1e-7)
+
+    def _createY(self, layer):
+        Y_shape = (3, layer.C)
+        Y = np.zeros(Y_shape)
+        Y[range(3), np.random.randint(layer.C, size=3)] = 1.
+        if not layer.is_multiclass:
+            Y = Y[:, 0:1]
+        return Y
+
+    def _createAPrev(self, layer):
+        A_prev_shape = (3, *layer.shape_in)
+        A_prev = np.random.normal(0, 1, A_prev_shape)
+        return A_prev
+
+    def _activatedLayerCost(self, A_prev, *weights):
+        layer = self._layer
+        layer.weights = weights
+        A = self._layer.forward(A_prev)
+        cost = np.sum(A)
+        return cost
+
+    def _activatedLayerGradients(self, A_prev, *weights):
+        layer = self._layer
+        layer.weights = weights
+        A = layer.forward(A_prev)
+        dA = np.ones(A.shape)
+        dA_prev, grads = layer.backward(dA, True)
+        return (dA_prev, *grads)
+
+    def _outputLayerCost(self, A_prev, *weights):
+        layer = self._layer
+        layer.weights = weights
+        layer.forward(A_prev)
+        return layer.cost
+
+    def _outputLayerGradients(self, A_prev, *weights):
+        layer = self._layer
+        layer.weights = weights
+        layer.forward(A_prev)
+        dA_prev, grads = layer.backward(None, True)
+        return (dA_prev, *grads)
+
+    ####
+    # Layer composition
+    ####
+
+    def testActivatedLayer_Persistence(self):
+        layer = nn_layers.dense(10, 4)
+        nn_dict = {}
+        layer.save_weights(nn_dict, 0)
+        layer.load_weights(nn_dict, 0)
+
+    def testOutputLayer_Predict(self):
+        layer = nn_layers.multiclass_output(10, 4)
+        A_prev = self._createAPrev(layer)
+        layer.forward(A_prev)
+        self.assertEqual(layer.pred.shape, (3,))
+        self.assertEqual(layer.prob.shape, (3, 4))
