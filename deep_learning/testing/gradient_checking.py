c8feb9f4991045e43725381e641c15a46b319fbe
Mike Wacker <11431865+mikewacker@users.noreply.github.com>
2022-01-30 17:21:17 -0500

    refactor gradient checking for clarity, add tests

--- deep_learning/testing/gradient_checking.py
+++ deep_learning/testing/gradient_checking.py
@@ -5,83 +5,99 @@ import numpy as np
 def check(cost_fn, grads_fn, *arrays):
     """Checks the computation of the gradients by approximating them.
 
+    Recall the formula to approximate the derivative of a function.
+
+    f'(x) = [f(x + eps) - f(x - eps)] / [(x + eps) - (x - eps)]
+          = [f(x + eps) - f(x - eps)] / [2 * eps]
+
+    The gradient approximation is essentially the multi-variable version,
+    where "f" is a cost function that outputs a single value.
+
     Args:
         cost_fn: computes the cost from the numpy arrays
-        grads_fn: computes the gradient for each numpy array
+        grads_fn: computes the actuals gradients from the numpy arrays
         arrays: numpy arrays to run the check with
 
     Returns:
-        diff: relative difference, which should be a small value
-        grads: computed gradients
-        grads_approx: approximated gradients, using the cost function
+        diff: relative difference, which should be small (ideally, < 1e-7)
+        grads: actual gradients
+        grads_approx: approximated gradients
     """
     grads = grads_fn(*arrays)
     grads_approx = _approximate_gradients(cost_fn, arrays)
-    diff = _compute_diff(grads, grads_approx)
+    diff = _compute_diff_arrays(grads, grads_approx)
     return diff, grads, grads_approx
 
 def _approximate_gradients(cost_fn, arrays, epsilon=1e-7):
     """Approximates the gradients."""
     return tuple(
-        _approximate_gradient(cost_fn, arrays, A_index, A, epsilon)
-        for A_index, A in enumerate(arrays))
+        _approximate_gradient(cost_fn, arrays, arrays_index, A, epsilon)
+        for arrays_index, A in enumerate(arrays))
 
-def _approximate_gradient(cost_fn, arrays, A_index, A, epsilon):
-    """Approximates the gradient for an array."""
+def _approximate_gradient(cost_fn, arrays, arrays_index, A, epsilon):
+    """Approximates the gradient for a single array."""
     dA_approx = np.empty(A.shape)
     for index in _get_indices(A):
         deriv_approx = _approximate_derivative(
-            cost_fn, arrays, A_index, A, index, epsilon)
+            cost_fn, arrays, arrays_index, index, epsilon)
         dA_approx[index] = deriv_approx
     return dA_approx
 
+def _approximate_derivative(f, arrays, arrays_index, index, epsilon):
+    """Approximates the derivative for a single element."""
+    arrays_plus = _create_shifted_copy(arrays, arrays_index, index, epsilon)
+    y_plus = f(*arrays_plus)
+    arrays_minus = _create_shifted_copy(arrays, arrays_index, index, -epsilon)
+    y_minus = f(*arrays_minus)
+    deriv_approx = (y_plus - y_minus) / (2 * epsilon)
+    return deriv_approx
+
 def _get_indices(A):
-    """Gets the indices for an array."""
+    """Gets the index for each element in the array."""
     dim_ranges = [range(size) for size in A.shape]
     if len(dim_ranges) == 1:
         return dim_ranges[0]
     return itertools.product(*dim_ranges)
 
-def _approximate_derivative(f, arrays, A_index, A, index, epsilon):
-    """Approximates the derivative for a single element."""
-    A_plus = _shift_element(A, index, epsilon)
-    arrays_plus = _merge_arrays(arrays, A_index, A_plus)
-    y_plus = f(*arrays_plus)
-    A_minus = _shift_element(A, index, -epsilon)
-    arrays_minus = _merge_arrays(arrays, A_index, A_minus)
-    y_minus = f(*arrays_minus)
-    deriv_approx = (y_plus - y_minus) / (2 * epsilon)
-    return deriv_approx
+def _create_shifted_copy(arrays, arrays_index, index, delta):
+    """Creates a copy of the arrays with one element shifted."""
+    A = arrays[arrays_index]
+    A_shifted = A.copy()
+    A_shifted[index] += delta
+    arrays_shifted = list(arrays)
+    arrays_shifted[arrays_index] = A_shifted
+    return arrays_shifted
 
-def _shift_element(A, index, delta):
-    """Shifts a single element in the array."""
-    A_shift = A.copy()
-    A_shift[index] += delta
-    return A_shift
+def _compute_diff_arrays(arrays1, arrays2):
+    """Computes the difference between two tuples of arrays"""
+    vector1 = _flatten(arrays1)
+    vector2 = _flatten(arrays2)
+    return _compute_diff_flat(vector1, vector2)
 
-def _merge_arrays(arrays, A_index, A):
-    """Merges a changed array back into the arrays."""
-    arrays = list(arrays)
-    arrays[A_index] = A
-    return tuple(arrays)
-
-def _compute_diff(grads, grads_approx):
-    """Computes the difference between the gradients and their approximation."""
-    grad = _flatten(grads)
-    grad_approx = _flatten(grads_approx)
-    diff = np.linalg.norm(grad_approx - grad)
-    diff_norm = np.linalg.norm(grad_approx) + np.linalg.norm(grad)
-    diff /= diff_norm
-    return diff
+def _compute_diff_flat(vector1, vector2):
+    """Computes the difference between two flat vectors"""
+    diff = np.linalg.norm(vector2 - vector1)
+    norm = np.linalg.norm(vector1) + np.linalg.norm(vector2)
+    diff_normed = diff / norm
+    return diff_normed
 
 def _flatten(arrays):
     """Flattens the arrays into a single vector."""
-    sizes = [np.prod(np.array(A.shape)) for A in arrays]
-    m = sum(sizes)
-    grad = np.empty(m)
+    splices = _get_flat_splices(arrays)
+    n = splices[-1][1]
+    vector = np.empty(n)
+    for (offset, end), A in zip(splices, arrays):
+        vector[offset:end] = A.reshape(-1)
+    return vector
+
+def _get_flat_splices(arrays):
+    """Gets each array's splice in the flat array."""
+    splices = []
     offset = 0
-    for A, size in zip(arrays, sizes):
+    for A in arrays:
+        size = np.prod(A.shape)
         end = offset + size
-        grad[offset:end] = A.reshape(-1)
+        splice = (offset, end)
+        splices.append(splice)
         offset = end
-    return grad
+    return splices
--- deep_learning/testing/gradient_checking_test.py
+++ deep_learning/testing/gradient_checking_test.py
@@ -1,9 +1,11 @@
 import unittest
+import numpy.testing as npt
 from . import gradient_checking
 
 import numpy as np
 
 class GradientCheckingTestCase(unittest.TestCase):
+    """Unit tests for gradient checking."""
 
     def testCheckGradients(self):
         W = np.array([[1.0, 2.0], [3.0, 4.0]])
@@ -12,12 +14,34 @@ class GradientCheckingTestCase(unittest.TestCase):
         self.assertLess(diff, 1e-7)
 
     def _cost(self, W, X):
+        """Sample cost function."""
         Y = W @ X
         cost = np.sum(Y)
         return cost
 
     def _grads(self, W, X):
+        """Sample gradients function."""
+        # If the cost is the sum of Y, then dY is an array of 1s.
         dY = np.ones((2, 1))
         dW = dY @ X.T
         dX = W.T @ dY
         return (dW, dX)
+
+    ###
+    # Subunit tests
+    ###
+
+    def testApproximateGradients(self):
+        arrays = (np.array([[1.0, 2.0]]), np.array([[3.0], [4.0]]))
+        cost_fn = lambda *arrays: sum(np.prod(A) for A in arrays)
+        grads = gradient_checking._approximate_gradients(cost_fn, arrays, 0.1)
+        expected_grads = (np.array([[2.0, 1.0]]), np.array([[4.0], [3.0]]))
+        for dA, expected_dA in zip(grads, expected_grads):
+            npt.assert_almost_equal(dA, expected_dA)
+
+    def testComputeDiff(self):
+        arrays1 = (np.array([[0.0, 1.0]]), np.array([[2.0], [3.0]]))
+        arrays2 = (np.array([[1.0, 1.0]]), np.array([[1.0], [1.0]]))
+        diff = gradient_checking._compute_diff_arrays(arrays1, arrays2)
+        expected_diff = np.sqrt(6) / (np.sqrt(14) + 2)
+        npt.assert_almost_equal(diff, expected_diff)
